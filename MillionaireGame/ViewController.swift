4b73fc9f24000bc6b6d213f55b890048dca190c8
Andrey91 <mistaandrey@icloud.com>
2022-11-27 23:47:31 +0500

    initiate project

---
+++ MillionaireGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MillionaireGame
+//
+//  Created by Andrey Piskunov on 27.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
